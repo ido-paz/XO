182f631d35e52350cd2419a8b1d2942921e6c31c
Ido Paz <ido_p_a@hotmail.com>
2020-12-30 18:42:06 +0200

    seperated logic from UI

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { XO } from './components/XO';
+import { Board } from './components/Board';
 
 ReactDOM.render(
   <React.StrictMode>
-    <XO />
+    <Board />
   </React.StrictMode>,
   document.getElementById('root')
 );
